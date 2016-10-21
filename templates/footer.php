1c1bd4790be121f862995bdcc2a95b3c563abcab
jeffnm <jeffmudge@gmail.com>
2016-10-21 09:56:59 -0500

    SQL Security Patch
    Security patch for sql injection vulnerability.

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 
 
-<div class="footer">Copyright &copy; 2016. Auth Module version 1.1<br/><a href="http://coral-erm.org/">CORAL Project Website</a> | <a href="https://github.com/ndlibersa/auth">GitHub Site</a></div>
+<div class="footer">Copyright &copy; 2016. Auth Module version 1.1.1<br/><a href="http://coral-erm.org/">CORAL Project Website</a> | <a href="https://github.com/ndlibersa/auth">GitHub Site</a></div>
 
 </body>
 </html>
